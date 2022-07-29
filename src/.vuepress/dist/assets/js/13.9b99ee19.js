c639af3e057b17caf2d768f76379c639e0f69d4b
sergey_zhuravlev <ser90593696.zhuravlev@yandex.ru>
2022-07-29 22:52:57 +0300

    test

--- src/.vuepress/dist/assets/js/13.9b99ee19.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{286:function(s,t,n){"use strict";n.r(t);var i={data:()=>({msg:"Hello this is <Foo-Bar>"})},l=n(16),o=Object(l.a)(i,(function(){return(0,this._self._c)("p",{staticClass:"demo"},[this._v("\n  "+this._s(this.msg)+" 222222222\n")])}),[],!1,null,null,null);t.default=o.exports}}]);
--- src/LINKS_FOR_LERN_VUEPRESS.md
+++
@@ -1,6 +0,0 @@
-https://www.8host.com/blog/vvedenie-v-vuepress-osnovnye-operacii-i-temy/
-
-https://habr.com/ru/post/580894/
-
-https://github.com/dev-pandaren/vue-press-sample
-
